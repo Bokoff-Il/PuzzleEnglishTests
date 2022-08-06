20b09a7383363f93eb3e0c1b4ff02ae643e3f296
Bokoff-Il <bokoff.il@gmail.com>
2022-08-06 20:41:43 +0300

    refactor specs

--- src/test/java/puzzleenglish/com/tests/config/ApiConfig.java
+++ src/test/java/puzzleenglish/com/tests/config/ApiConfig.java
@@ -4,6 +4,7 @@ import org.aeonbits.owner.Config;
 
 @Config.Sources("classpath:config/api.properties")
 public interface ApiConfig extends Config {
+    @DefaultValue("https://puzzle-english.com")
     String baseUri();
 
     String cookieName();
--- src/test/java/puzzleenglish/com/tests/tests/api/specs/Specs.java
+++ src/test/java/puzzleenglish/com/tests/tests/api/specs/Specs.java
@@ -1,7 +1,5 @@
 package puzzleenglish.com.tests.tests.api.specs;
 
-import static io.restassured.RestAssured.with;
-
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.specification.RequestSpecification;
@@ -9,22 +7,23 @@ import io.restassured.specification.ResponseSpecification;
 import org.aeonbits.owner.ConfigFactory;
 import puzzleenglish.com.tests.config.ApiConfig;
 
+import static io.restassured.RestAssured.with;
 import static io.restassured.http.ContentType.JSON;
 import static puzzleenglish.com.tests.tests.api.helpers.CustomApiListener.withCustomTemplates;
 
 public class Specs {
-    static String baseUri = System.getProperty("baseUri", "https://puzzle-english.com");
     static ApiConfig apiConfig = ConfigFactory.create(ApiConfig.class);
+
     public static RequestSpecification requestUsers = with()
             .filter(withCustomTemplates())
-            .baseUri(baseUri)
+            .baseUri(apiConfig.baseUri())
             .basePath("/api2")
             .cookie(apiConfig.cookieName(), apiConfig.cookieValue())
             .contentType(JSON);
 
     public static RequestSpecification requestComments = with()
             .filter(withCustomTemplates())
-            .baseUri(baseUri)
+            .baseUri(apiConfig.baseUri())
             .basePath("/api2/comments")
             .cookie(apiConfig.cookieName(), apiConfig.cookieValue())
             .contentType("application/x-www-form-urlencoded");
